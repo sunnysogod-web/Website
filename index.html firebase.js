8d4632a8a04d91584272ec3d5408cabf6674258a
sunnysogod-web <sunnysogod@gmail.com>
2026-02-11 21:22:22 +0700

    Create index.html firebase.js

---
+++ index.html firebase.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCm9QsD6NwRF0zusMzgESEyA43hjEyHRhw",
+  authDomain: "sw-website-68.firebaseapp.com",
+  databaseURL: "https://sw-website-68-default-rtdb.firebaseio.com",
+  projectId: "sw-website-68",
+  storageBucket: "sw-website-68.firebasestorage.app",
+  messagingSenderId: "1037550377013",
+  appId: "1:1037550377013:web:28d2f983c552a118e9397c",
+  measurementId: "G-ESPPG76N3H"
+};
+
+
